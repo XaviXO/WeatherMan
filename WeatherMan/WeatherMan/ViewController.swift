e80ea615d0f2be9d595d90cf0af3b0a6a8209666
Jaxx Rockefeller <47236441+XaviXO@users.noreply.github.com>
2020-01-17 14:27:09 -0800

    Add files via upload

---
+++ WeatherMan/WeatherMan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherMan
+//
+//  Created by Javier Calderon Jr. on 1/17/20.
+//  Copyright © 2020 RockefellerMagic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
